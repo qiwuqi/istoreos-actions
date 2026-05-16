9017f4f3fd0f76b270481df326e1e072f162dee3
qiwuqi <66163004+qiwuqi@users.noreply.github.com>
2026-05-17 02:34:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-
-# 修改管理IP（唯一安全修改）
+# 修改管理IP
 sed -i 's/192.168.100.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
-# ==================== 纯净默认WiFi ====================
+# 默认WiFi
 mkdir -p files/etc/config
-
 cat > files/etc/config/wireless << EOF
 config wifi-device 'radio0'
         option type 'mac80211'
